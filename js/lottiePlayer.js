f363f3753d4e591ae3dcb0baa6e08a90046802d2
Bogdan-Branzaniuc <branzaniucbogdan20@gmail.com>
2022-11-01 07:37:00 +0000

    update

--- js/lottiePlayer.js
+++
@@ -1,9 +0,0 @@
-LottieInteractivity.create({
-    mode: 'scroll',
-    player: '#firstLottie',
-    actions: [{
-        visibility: [0, 1],
-        type: 'seek',
-        frames: [0, 100],
-    }, ],
-});
