32f39c111a34260994320ce48e9a00d172b4b6fe
Bogdan-Branzaniuc <branzaniucbogdan20@gmail.com>
2022-11-10 13:08:47 +0000

    never have I ever done spaghetti code (guilty) -added scrollbar-and-nav.css to make the side makfile e tmake the side-scroll-bar responsive for landscape and portrait orientation, updated the scroll. updated the scroll.js file to deal with the change of orientation, froze a big part of the media querries in order to solve it

--- js/scorll.js
+++ js/scorll.js
@@ -2,29 +2,35 @@ let screenOrientation
 
 window.addEventListener("load", () => {
     screenOrientation = screen.orientation.type
-    console.log(screenOrientation)
+    setMenuLinkHeight()
 });
 
 screen.orientation.addEventListener("change", () => {
     screenOrientation = screen.orientation.type
     console.log(screenOrientation)
+    setMenuLinkHeight()
 });
 
+//****mdm-webdocs code
 let supportsOrientationChange = "onorientationchange" in window,
     orientationEvent = supportsOrientationChange ? "orientationchange" : "resize";
 
 window.addEventListener(orientationEvent, function () {
     window.location.reload()
 }, false);
+//**** 
+
 
 function scrollProgress() {
     const scroll = document.body.scrollTop || document.documentElement.scrollTop;
     const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
     const scrolled = (scroll / height) * 100;
-
     if (screenOrientation === "portrait-primary") {
+        console.log(123)
+        document.getElementById("scroll-bar").style.width = "100%"
         document.getElementById("scroll-bar").style.height = scrolled + "%";
     } else {
+
         document.getElementById("scroll-bar").style.width = scrolled + "%";
     }
 }
@@ -52,8 +58,10 @@ function setMenuLinkHeight() {
         } else {
             if (screenOrientation === "portrait-primary") {
                 li.style.height = liSize + "%"
+                li.style.width = "100%"
             } else {
                 li.style.width = liSize + "%"
+                li.style.height = "100%"
             }
         }
     }
