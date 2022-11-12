6c5b69043f0220f9b78d91171ffdbf421c01a29f
Bogdan-Branzaniuc <branzaniucbogdan20@gmail.com>
2022-11-12 14:56:31 +0000

    finished the side-menu-scroll-bar, made it responsive and cleaned the code

--- js/scorll.js
+++ js/scorll.js
@@ -1,50 +1,41 @@
-let screenOrientation
+function responsiveToResizing() {
 
-window.addEventListener("load", () => {
-    screenOrientation = screen.orientation.type
-    setMenuLinkHeight()
-});
+    let screenOrientation
 
-screen.orientation.addEventListener("change", () => {
-    screenOrientation = screen.orientation.type
-    console.log(screenOrientation)
-    setMenuLinkHeight()
-});
+    window.onload = () => {
+        if (window.innerHeight > window.innerWidth) {
+            screenOrientation = "portrait-primary"
+        } else {
+            screenOrientation = "landscape-primary"
+        }
+        setMenuLinkHeight(screenOrientation)
+    }
+    window.addEventListener("resize", () => {
+        window.location.reload()
+    })
+    window.onresize = () => window.location.reload()
 
-//****mdm-webdocs code
-let supportsOrientationChange = "onorientationchange" in window,
-    orientationEvent = supportsOrientationChange ? "orientationchange" : "resize";
+    window.onscroll = () => scrollProgress(screenOrientation);
+}
 
-window.addEventListener(orientationEvent, function () {
-    window.location.reload()
-}, false);
-//**** 
-
-
-function scrollProgress() {
+function scrollProgress(screenOrientation) {
     const scroll = document.body.scrollTop || document.documentElement.scrollTop;
     const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
     const scrolled = (scroll / height) * 100;
     if (screenOrientation === "portrait-primary") {
-        console.log(123)
         document.getElementById("scroll-bar").style.width = "100%"
         document.getElementById("scroll-bar").style.height = scrolled + "%";
     } else {
-
         document.getElementById("scroll-bar").style.width = scrolled + "%";
     }
 }
 
-window.onscroll = () => scrollProgress();
-
-
-function setMenuLinkHeight() {
+function setMenuLinkHeight(screenOrientation) {
     const liElements = document.querySelectorAll("#side-scroll-menu li")
     const body = document.body;
     const html = document.documentElement;
     const documentHeight = Math.max(body.scrollHeight, body.offsetHeight,
         html.clientHeight, html.scrollHeight, html.offsetHeight);
-    console.log(documentHeight)
     const offsetScrollIntoView = 150
 
     for (let li of liElements) {
@@ -66,4 +57,4 @@ function setMenuLinkHeight() {
     }
 }
 
-setMenuLinkHeight()
+responsiveToResizing()
