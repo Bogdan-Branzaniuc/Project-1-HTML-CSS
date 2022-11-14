8df5f0339ae2d4cd577d213fe356236d82f2064f
Bogdan-Branzaniuc <branzaniucbogdan20@gmail.com>
2022-11-14 04:38:10 +0000

    all home page but footer is fully responsive, added lottie-animations to the sectionions (high chance to be cahnged)

---
+++ assets/js/lottie-players.js
@@ -0,0 +1,19 @@
+// LottieInteractivity.create({
+//     player: '#fusion-lottie',
+//     mode: 'scroll',
+//     actions: [{
+//         visibility: [0, 1],
+//         type: 'seek',
+//         frames: [0, 70],
+//     }, ]
+// });
+
+LottieInteractivity.create({
+    player: '#fission-lottie',
+    mode: 'scroll',
+    actions: [{
+        visibility: [0, 1],
+        type: 'seek',
+        frames: [0, 450],
+    }, ]
+});
