acc9037226e062a7ecdf5ecd7647f0a62de88e6b
Bogdan-Branzaniuc <branzaniucbogdan20@gmail.com>
2022-11-16 15:23:41 +0000

    verified and validated code, made the suitable changes

--- assets/js/click-burger-icon.js
+++ assets/js/click-burger-icon.js
@@ -1,14 +1,14 @@
-const mainNavMenu = document.querySelector(`#main-nav #menu`)
-const burgerMenu = document.querySelector('#main-nav #burger-icon')
-const burgerMenuIcon = document.querySelector('#main-nav #burger-icon>i')
+const mainNavMenu = document.querySelector('#main-nav #menu');
+const burgerMenu = document.querySelector('#main-nav #burger-icon');
+const burgerMenuIcon = document.querySelector('#main-nav #burger-icon>i');
 
-let burgerMenuClicked = window.width <= 1200 ? false : true
+let burgerMenuClicked = window.width <= 1200 ? false : true;
 
 burgerMenu.onclick = function (event) {
-    event.stopPropagation
-    console.log(burgerMenuClicked)
-    mainNavMenu.style.display = burgerMenuClicked ? "flex" : "none"
-    burgerMenuIcon.style.scale = burgerMenuClicked ? "1.3" : "1"
+    event.stopPropagation;
+    console.log(burgerMenuClicked);
+    mainNavMenu.style.display = burgerMenuClicked ? "flex" : "none";
+    burgerMenuIcon.style.scale = burgerMenuClicked ? "1.3" : "1";
 
     burgerMenuClicked = !burgerMenuClicked;
 }
--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -3,19 +3,15 @@ function scrollProgress(screenOrientation) {
     const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
     const scrolled = (scroll / height) * 100;
     if (screenOrientation === "portrait-primary") {
-        document.getElementById("scroll-bar").style.width = "100%"
+        document.getElementById("scroll-bar").style.width = "100%";
         document.getElementById("scroll-bar").style.height = scrolled + "%";
     } else {
         document.getElementById("scroll-bar").style.width = scrolled + "%";
     }
 }
 
-function createOffsetScrollIntoViewDivs(id, icon) {
-
-}
-
 function setMenuLinkHeight(screenOrientation) {
-    const liElements = document.querySelectorAll("#side-scroll-menu li")
+    const liElements = document.querySelectorAll("#side-scroll-menu li");
     const body = document.body;
     const html = document.documentElement;
     const documentHeight = Math.max(body.scrollHeight, body.offsetHeight,
@@ -23,38 +19,38 @@ function setMenuLinkHeight(screenOrientation) {
     const offsetScrollIntoView = 150
 
     for (let li of liElements) {
-        let sectionId = li.firstChild.hash
+        let sectionId = li.firstChild.hash;
 
-        const sectionHeight = document.querySelector(`${sectionId}`).offsetHeight
-        let liSize = Math.floor(((sectionHeight + offsetScrollIntoView) / documentHeight) * 100)
+        const sectionHeight = document.querySelector(`${sectionId}`).offsetHeight;
+        let liSize = Math.floor(((sectionHeight + offsetScrollIntoView) / documentHeight) * 100);
 
         if (liSize == Infinity) {
-            li.style.height = 0
+            li.style.height = 0;
         } else {
             if (screenOrientation === "portrait-primary") {
-                li.style.height = liSize + "%"
-                li.style.width = "100%"
+                li.style.height = liSize + "%";
+                li.style.width = "100%";
             } else {
-                li.style.width = liSize + "%"
-                li.style.height = "100%"
+                li.style.width = liSize + "%";
+                li.style.height = "100%";
             }
         }
     }
 }
 
 function responsiveToResizing() {
-    let screenOrientation
+    let screenOrientation;
     window.onload = () => {
         if (window.innerHeight > window.innerWidth) {
-            screenOrientation = "portrait-primary"
+            screenOrientation = "portrait-primary";
         } else {
-            screenOrientation = "landscape-primary"
+            screenOrientation = "landscape-primary";
         }
-        setMenuLinkHeight(screenOrientation)
-    }
-    window.onorientationchange = () => window.location.reload()
+        setMenuLinkHeight(screenOrientation);
+    };
+    window.onorientationchange = () => window.location.reload();
     window.onscroll = () => scrollProgress(screenOrientation);
 
 }
 
-responsiveToResizing()
+responsiveToResizing();
