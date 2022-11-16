28e5d89fd79279476bba2ade10c6f405fd4ec030
Bogdan-Branzaniuc <branzaniucbogdan20@gmail.com>
2022-11-16 08:09:01 +0000

    tokamak animation nested in Adobe After effects for a ligther json file, deleted (in-depth, progress and feedback) pages and added a maintenance page instead, for time and quality reasons

--- assets/js/click-burger-icon.js
+++ assets/js/click-burger-icon.js
@@ -2,8 +2,6 @@ const mainNavMenu = document.querySelector(`#main-nav #menu`)
 const burgerMenu = document.querySelector('#main-nav #burger-icon')
 const burgerMenuIcon = document.querySelector('#main-nav #burger-icon>i')
 
-//this method feels less consistent as if I would change the @media in the css files I could run into problems, 
-// It works for the time being
 let burgerMenuClicked = window.width <= 1200 ? false : true
 
 burgerMenu.onclick = function (event) {
--- assets/js/lottie-players.js
+++ assets/js/lottie-players.js
@@ -1,13 +1,3 @@
-// LottieInteractivity.create({
-//     player: '#fusion-lottie',
-//     mode: 'scroll',
-//     actions: [{
-//         visibility: [0, 1],
-//         type: 'seek',
-//         frames: [0, 70],
-//     }, ]
-// });
-
 LottieInteractivity.create({
     player: '#fission-lottie',
     mode: 'scroll',
